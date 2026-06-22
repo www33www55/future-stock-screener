362e9e6cc0d11f6081e5fa385828c98e17079fa2
www33www55 <lovebetty521i@icloud.com>
2026-06-22 23:23:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 
-st.set_page_config(page_title="90分選股器")
+st.set_page_config(page_title="未來小股神90分選股器")
 
-st.title("🚀 未來小股神 90分選股器")
+st.title("🚀 未來小股神90分選股器")
 
-st.success("建立成功")
+st.success("90分選股器啟動成功")
+
+stocks = [
+    "2330.TW",
+    "2303.TW",
+    "2409.TW",
+    "2313.TW",
+    "3037.TW",
+]
+
+st.subheader("今日觀察清單")
+
+for s in stocks:
+    st.write("📈", s)
